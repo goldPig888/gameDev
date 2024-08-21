e2a0076fe8f971a2e5a38f01dd681b435011dc37
may <maylynnespinola@gmail.com>
2024-08-21 11:05:45 -0500

    5 and 6

---
+++ projects/CH1toCH2/CH1toCH2/GD1_CH5to6.cs
@@ -0,0 +1,11 @@
+﻿public class GD1_CH5to6
+{
+    public static long NthFibonacci(long p)
+    {
+        return (1 / Math.Sqrt(5) * Math.Pow(((1 + Math.Sqrt(5) / 2), p-1) - 1 / Math.Sqrt(5) * Math.Pow(((1 - Math.Sqrt(5) / 2), p - 1);
+    }
+    public static Boolean IsLeapYear(int year)
+    {
+
+    }
+}
