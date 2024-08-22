3896ca962d0affc4f957e8d36b6ee2263c1e7a2d
may <136521048+goldPig888@users.noreply.github.com>
2024-08-21 23:52:06 -0500

    fixed

--- projects/CH1toCH2/CH1toCH2/GD1_CH5to6.cs
+++ projects/CH1toCH2/CH1toCH2/GD1_CH5to6.cs
@@ -2,10 +2,26 @@
 {
     public static long NthFibonacci(long p)
     {
-        return (1 / Math.Sqrt(5) * Math.Pow(((1 + Math.Sqrt(5) / 2), p-1) - 1 / Math.Sqrt(5) * Math.Pow(((1 - Math.Sqrt(5) / 2), p - 1);
+        if (p <= 0) return -1;
+        double a = (1 + Math.Sqrt(5)) / 2;
+        double b = (1 - Math.Sqrt(5)) / 2;
+        return (long)Math.Round((Math.Pow(a, p - 1) - Math.Pow(b, p - 1)) / Math.Sqrt(5));
     }
-    public static Boolean IsLeapYear(int year)
-    {
 
+    public static bool IsLeapYear(int year)
+    {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+
+    public static int DaysInMonth(int month, int year)
+    {
+        return DateTime.DaysInMonth(year, month);
+    }
+
+    public static int DaysBetween(int startMonth, int startDay, int startYear, int endMonth, int endDay, int endYear)
+    {
+        DateTime startDate = new DateTime(startYear, startMonth, startDay);
+        DateTime endDate = new DateTime(endYear, endMonth, endDay);
+        return (endDate - startDate).Days;
     }
 }
---
+++ projects/CH1toCH2/CH1toCH2/NthFibonacci_Tests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class NthFibonacci_Tests
+{
+    [SetUp]
+    public void SetUp()
+    {
+        // Setup code here (if required)
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Teardown code here (if required)
+    }
+
+    [Test]
+    public void Test1()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(1);
+        long expected = 0;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(2);
+        long expected = 1;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(3);
+        long expected = 1;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test4()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(4);
+        long expected = 2;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test5()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(5);
+        long expected = 3;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test6()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(6);
+        long expected = 5;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test7()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(7);
+        long expected = 8;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test8()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(30);
+        long expected = 514229;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test9()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(50);
+        long expected = 7778742049;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test10()
+    {
+        long actual = GD1_CH5to6.NthFibonacci(67);
+        long expected = 27777890035288;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
+
