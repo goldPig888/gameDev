a0ef8d949cef1f88971084f2731019252b1b0426
may <maylynnespinola@gmail.com>
2024-08-28 11:50:14 -0500

    Update GD2_CH1to3.cs

--- projects/GD2/GD2_CH1to3.cs
+++ projects/GD2/GD2_CH1to3.cs
@@ -9,13 +9,51 @@
         }
         return (sum, sum * 1.0825f);
     }
-  /*public static (string[], string[], float, float) EvaluateScore((string, int)[] students)
+
+  public static (string[], string[], float, float) EvaluateScores((string, int)[] students)
     {
-        foreach (Student s in students)
+        string[] all = new string[students.Length];
+        string[] pass = new string[students.Length];
+        float avg = 0;
+
+        foreach ((string name, int score) in students)
         {
-            console.WriteLine(s); 
+            all.Append(name);
+            if (score >= 3) { pass.Append(name); }
+            avg += score;
         }
-    }*/
+
+        return (all, pass, all.Length / students.Length, avg);
+    }
+
+    public static int Gather(char[] farm)
+    {
+
+        int left = 0, right = 0, steps = 0;
+        int start = Array.IndexOf(farm, 'H');
+
+        while (left != 0)
+        {
+            if (farm[left] == 'F') {
+                steps += (start - left * 2);
+                farm[left] = '-';
+            }
+            left++;
+
+        }
+
+        while (right != farm.Length-1)
+        {
+            if (farm[right] == 'F')
+            {
+                steps += (right * 2);
+                farm[right] = '-';
+            }
+            right++;
+
+        }
+        return steps;
 
 
+    }
 }
