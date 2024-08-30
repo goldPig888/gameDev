aaa8847e4a2dc0971b5ac27d15f4ba937528609c
may <maylynnespinola@gmail.com>
2024-08-30 11:28:05 -0500

    merging the ring and build

--- projects/GD2/GD2_CH7to9.cs
+++ projects/GD2/GD2_CH7to9.cs
@@ -1,6 +1,26 @@
 ﻿public class GD2_CH7to9
 {
-    public static int[] RingTotals(int[,] grid)
+    public static List<Tuple<int, int>> BuildingPlacement(int width, int height, bool[,] grid)
+    {
+        var validPlacements = new List<Tuple<int, int>>();
+        int rows = grid.GetLength(0), cols = grid.GetLength(1);
+
+        for (int i = 0; i <= rows - height; i++)
+        {
+            for (int j = 0; j <= cols - width; j++)
+            {
+                bool isValid = true;
+                for (int h = 0; h < height && isValid; h++)
+                    for (int w = 0; w < width; w++)
+                        if (grid[i + h, j + w]) { isValid = false; break; }
+                if (isValid) validPlacements.Add(new Tuple<int, int>(j, i));
+            }
+        }
+
+        return validPlacements;
+        }
+    /*
+public static int[] RingTotals(int[,] grid)
     {
 
         for (int row = grid.GetLength(0); row >= 0; row--)
@@ -13,5 +33,5 @@
             
         }
 
-    }
+    }*/
 }
