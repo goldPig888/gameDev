7d1dca731f59d5f197f0eb6cb7ecfd499d331511
may <maylynnespinola@gmail.com>
2024-10-10 11:50:06 -0500

    the connect4

--- projects/unity/connectFour/Assets/Scripts/GameManager.cs
+++ projects/unity/connectFour/Assets/Scripts/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // load a variable to keep track of who's turns it is, switch between 1 and -1 by multiplying by -1
+    // red starts first at 1 and blank is -1
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //track mouse position in update for hovering above piece
+    //on click drop the piece
+
+    //then check win fancily
+    //use raycast to see if there are any of the same sprites adjacent
+
+    //see how many pathways to check
+    //if there is then keep going
+    //if we reach 4 then win
+    //keep checking the other pathways
+
+    //if pieces number == 42 then game over since thats max out spots and nobody has won
 }
