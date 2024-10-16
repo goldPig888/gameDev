693f8bb2045258d074e6c73c2f36d031638d947a
may <maylynnespinola@gmail.com>
2024-10-16 12:03:47 -0500

    rushhour2

---
+++ projects/unity/rushHour2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //when you click with your mouse on a sprite car, check the tag, it will either be x or y, then move when ur mouse and itll follow 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
